5ad81bc8cedc08878b20df5346abdc7bd3498b2c
Elizabeth <elizabethsong65@yahoo.com>
2020-03-21 20:08:35 -0400

    Add existing files from self-practice

---
+++ blackjack.py
@@ -0,0 +1,32 @@
+"""User Story: As a user I want to clearly see the cards dealt to me and to the dealer.
+
+User Story: As a user I want to be able to choose to stay.
+
+User Story: As a user I want to be able to choose to hit until I choose to stay or until I bust.
+
+User Story: As a user I want to clearly see the sequence of moves made by the dealer.
+
+User Story: As a user I want to clearly see who won and why.
+
+User Story: As a user I want to be able to quit the game or go again after each cycle."""
+
+"""Intermediate Challenge
+
+User Story: As a user I want the game to incorporate a virtual chip/currency system to simulate real-life staking mechanics.
+
+User Story: As a user I want to clearly see my chip stack, as well as that of the dealer.
+
+User Story: As a user I want to be able to vary my bet according to the min/max betting rules.
+
+Advanced Challenge
+
+User Story: As a user I want to be able to choose to split my hand whenever possible.
+
+User Story: As a user I want to be able to choose to double down whenever possible."""
+
+from stack import Stack
+import random
+
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
+#User Story: As a user I want to clearly see the cards dealt to me and to the dealer.
+print("User\n")
---
+++ stack.py
@@ -0,0 +1,55 @@
+from node import Node
+
+class Stack:
+
+  #Stacks can only view and make changes to the top node in the stack.
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.size = 0
+    #To prevent stack overflow, it is necessary to limit the number of nodes that are added to the stack.
+    self.limit = limit
+  
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+      print("Adding {} to the pizza stack!".format(value))
+    else:
+      print("No room for {}!".format(value))
+
+  def pop(self):
+    if not self.is_empty():
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      print("Delivering " + item_to_remove.get_value())
+      return item_to_remove.get_value()
+    print("All out of pizza.")
+
+  def peek(self):
+    if not self.is_empty():
+      return self.top_item.get_value()
+    print("Nothing to see here!")
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self):
+    return self.size == 0
+
+  #Other funcs
+  def get_size(self):
+    return self.size
+  
+  def print_items(self):
+    pointer = self.top_item
+    print_list = []
+    while(pointer):
+      print_list.append(pointer.get_value())
+      pointer = pointer.get_next_node()
+    print_list.reverse()
+    print("{0} Stack: {1}".format(self.get_name(), print_list))
+
+#Making my own Stack
