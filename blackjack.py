b6dbef0d2b8314558f1f3a0237c249cb07955c8d
Elizabeth <elizabethsong65@yahoo.com>
2020-03-22 15:43:11 -0400

    completed game

--- blackjack.py
+++ blackjack.py
@@ -1,15 +1,5 @@
 #GUIDELINES FOR BLACKJACK
-"""User Story: As a user I want to clearly see the cards dealt to me and to the dealer.
 
-User Story: As a user I want to be able to choose to stay.
-
-User Story: As a user I want to be able to choose to hit until I choose to stay or until I bust.
-
-User Story: As a user I want to clearly see the sequence of moves made by the dealer.
-
-User Story: As a user I want to clearly see who won and why.
-
-User Story: As a user I want to be able to quit the game or go again after each cycle."""
 
 """Intermediate Challenge
 
@@ -30,66 +20,79 @@ from stack import Stack
 import random
 
 #VARIABLES
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']*4
-user = []
-dealer = []
+allcards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']*4
 
-#FUNCTIONS --> METHODS
-#class Hand:
-def calculateSum(hand):
-    """
-    Input: hand is a list of cards in user's hand
-    Calculates the sum of cards in the user or dealer's hand
-    Returns integer sum 
-    """
-    sum = 0
-    for card in hand:  
-        if card in ['J','Q','K']:
-             sum += 10
-        elif card == 'A':
-            if sum >= 11: #when sum is greater than 11, Ace is no longer 11 because then it would bust the hand
-                sum+= 1
+#CLASS PLAYER WITH METHODS
+class Player:
+    def __init__(self):
+        self.cards = []
+    
+    def __str__(self):
+        mycards = ""
+        for card in self.cards:
+            mycards += str(card) + " "
+        return mycards
+
+    def calculateSum(self):
+        """
+        Calculates the sum of cards in the user or dealer's hand
+        Returns integer sum 
+        """
+        sum = 0
+        for card in self.cards:  
+            if card in ['J','Q','K']:
+                sum += 10
+            elif card == 'A':
+                if sum >= 11: #when sum is greater than 11, Ace is no longer 11 because then it would bust the hand
+                    sum+= 1
+                else:
+                    sum+= 11
             else:
-                sum+= 11
+                sum += card
+        return sum
+
+    def hit(self):
+        """
+        Gives person a new card (adds it to the list of cards in their hand)
+        Unless they bust (over 21)
+        """
+        if not self.didBust():
+            newcard = allcards.pop()
+            self.cards.append(newcard)
+
         else:
-            sum += card
-    return sum
+            print("Sorry, you can't hit anymore. You have busted.")
+        return self.cards
 
-def hit(hand):
-    """
-    Input: hand is a list of cards in user's hand
-    Gives person a new card (adds it to the list of cards in their hand)
-    Unless they bust (over 21)
-    """
-    if not didBust(hand):
-        newcard = cards.pop()
-        hand.append(newcard)
+    def didBust(self):
+        """Input: hand is a list of cards in user's hand
+        Return boolean true if exceed 21
+        """
+        return self.calculateSum() > 21
 
-    else:
-        print("Sorry, you can't hit anymore. You have busted.")
-    return hand
-
-def didBust(hand):
-    """Input: hand is a list of cards in user's hand
-    Return boolean true if exceed 21
-    """
-    return calculateSum(hand) > 21
+   
 
 
 #GAME
 
 #User Story: As a user I want to clearly see the cards dealt to me and to the dealer.
 def blackjack():
-    random.shuffle(cards)
-    print(cards)
+    #create Player objects at start of game
+    user = Player()
+    dealer = Player()
+    random.shuffle(allcards)
+    print(allcards)
+
+    #Deal 2 cards to user
     print("User")
     for i in range(2):
-        hit(user)
+        user.hit()
     print(user)
 
+    #Deal 2 cards to dealer
     print("Dealer")
     for i in range(2):
-        hit(dealer)
+        dealer.hit()
     print(dealer)
 
     #User Story: As a user I want to be able to choose to stay.
@@ -97,39 +100,39 @@ def blackjack():
     #User Story: As a user I want to clearly see the sequence of moves made by the dealer.
 
     while True:
-        print("User total: " + str(calculateSum(user)))
-        print("Dealer total: " + str(calculateSum(dealer)))
+        print("User total: " + str(user.calculateSum()))
+        print("Dealer total: " + str(dealer.calculateSum()))
         choice = raw_input("Do you want to stay? Y or N. \n \n")
         if choice == "Y":
-            dealerTotal = calculateSum(dealer)
+            dealerTotal = dealer.calculateSum()
             if dealerTotal>=17:
                 #Dealer stays
-                if calculateSum(user)>calculateSum(dealer):
+                if user.calculateSum>dealer.calculateSum():
                     print("You win.")
                     break
-                elif calculateSum(user)==calculateSum(dealer):
+                elif user.calculateSum==dealer.calculateSum():
                     print("You tied.")
                     break
                 else:
                     print("Sorry! You lost.")
                     break
             else:
-                print(hit(dealer))
-                print("User total: " + str(calculateSum(user)))
-                print("Dealer total: " + str(calculateSum(dealer)))
-                if didBust(dealer):
+                print(dealer.hit())
+                print("User total: " + str(user.calculateSum()))
+                print("Dealer total: " + str(dealer.calculateSum()))
+                if dealer.didBust():
                     print("DEALER BUSTED! You win.")
                     break
         elif choice == "N":
-            print(hit(user))
-            print("User total: " + str(calculateSum(user)))
-            if didBust(user):
+            print(user.hit())
+            print("User total: " + str(user.calculateSum()))
+            if user.didBust():
                 print("USER BUSTED! You lose.")
                 break
             else:
-                print(hit(dealer))
-                print("Dealer total: " + str(calculateSum(dealer)))
-                if didBust(dealer):
+                print(dealer.hit())
+                print("Dealer total: " + str(dealer.calculateSum()))
+                if dealer.didBust():
                     print("DEALER BUSTED! You win.")
                     break
     
